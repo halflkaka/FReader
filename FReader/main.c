a63060403c6897b9e5ac716cc4c1ecdaaa2d39d0
侍粲杰 <shicanjie@shicanjiedemacbook-pro.local>
2017-12-02 22:51:20 +0800

    Initial Commit

---
+++ FReader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FReader
+//
+//  Created by 侍粲杰 on 2017/12/2.
+//  Copyright © 2017年 侍粲杰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
